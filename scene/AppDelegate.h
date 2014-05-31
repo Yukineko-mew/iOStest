010105d2902fb48a9572675c34955c289af80b1b
Yukineko <yukineko.mew7@gmail.com>
2014-05-31 18:56:43 +0900

    Initial Commit

---
+++ scene/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  scene
+//
+//  Created by Yukineko on 2014/05/31.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ scene/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  scene
+//
+//  Created by Yukineko on 2014/05/31.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
